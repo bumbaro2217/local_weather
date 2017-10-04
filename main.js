1ee098008543c1a02848d11f245837d20f3f9f0a
bumbaro2217 <byron2217@gmail.com>
2017-10-03 22:01:01 -0400

    finished up jquery, toggle working and background index OK

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-// api key 3d6919a94e3e99643aaed12a43c4cb86
+// api key appid=3d6919a94e3e99643aaed12a43c4cb86
 
 $(function() {
 
     var C = false;
     var apiData;
+    var appid = "3d6919a94e3e99643aaed12a43c4cb86";
 
     backgroundIMG = [
         "https://23711-presscdn-pagely.netdna-ssl.com/wp-content/uploads/2015/06/Thunderstorm-5best.jpg",
@@ -15,12 +16,22 @@ $(function() {
         "https://static.pexels.com/photos/1818/sky-clouds-cloudy-forest.jpg"
     ]
 
-    function showTemp(Fheit,Celsius) {
-        if(Celsius) return Math.round((Fheit-32)*(5/9)) + "&deg; C";
-        return Math.round(Fheit) + "&deg; F";
+    function showTemp(F,C) {
+        if(C) return Math.round((F-32)*(5/9)) + "&deg; C";
+        return Math.round(F) + "&deg; F";
     }
 
-    // API function
+    function render(data,C){
+        var currentWeather = data.weather[0].description
+        var currentTemp = showTemp(data.main.temp,C);
+        var icon = data.weather[0].icon;
+
+        $("#currentTemp").html(currentTemp);
+        $("#currentWeather").html(currentWeather);
+
+        var weatherIcon = "https://openweathermap.org/img/w/" + icon + ".png";
+        $("#currentTemp").prepend("<img src=" + weatherIcon + ">");
+    }
 
     $.getJSON("https://freegeoip.net/json/").done(function(location) {
         // console.log(location);
@@ -28,8 +39,30 @@ $(function() {
         $("#city").html(location.city);
         $("#latitude").html(location.latitude);
         $("#longitude").html(location.longitude);
+
+
+        $.getJSON("https://api.openweathermap.org/data/2.5/weather?lat="+location.latitude+
+        "&lon="+location.longitude+"&units=imperial&appid="+appid, function(data) {
+            apiData = data;
+            //console.log(apiData); WORKING!
+
+            render(apiData,C);
+
+            $("#toggle").click(function() {
+                C = !C;
+                render(data,C);
+            })
+
+            var id = data.weather[0].id,
+                    bgIndex,
+                    backgroundId = [299,499,599,699,799,800];
+            backgroundId.push(id);
+            bgIndex = backgroundId.sort().indexOf(id);
+
+            $("body").css("background-image", "url(" + backgroundIMG[bgIndex] + ")");
+
+        })
+    
     })
 
-
-
-})
+});
